fb3d3919f4a2a8e772c4125f2458cfedaa9c6618
Bhaskar2223 <133865412+Bhaskar2223@users.noreply.github.com>
2023-07-11 23:06:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 #this file consists of code for instances and sg
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIASSAAHTGBHY5Y4HAM"
-secret_key = "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"
+access_key = "AKIASSAAHTGBPAFLGSYW"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-03c7d01cf4dedc891"
+  ami             = "ami-04823729c75214919"
   instance_type   = "t2.micro"
   key_name        = "Rds"
   vpc_security_group_ids = [aws_security_group.five.id]
@@ -25,7 +25,7 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-03c7d01cf4dedc891"
+  ami             = "ami-04823729c75214919"
   instance_type   = "t2.micro"
   key_name        = "Rds"
   vpc_security_group_ids = [aws_security_group.five.id]
@@ -44,7 +44,7 @@ EOF
 }
 
 resource "aws_instance" "three" {
-  ami             = "ami-03c7d01cf4dedc891"
+  ami             = "ami-04823729c75214919"
   instance_type   = "t2.micro"
   key_name        = "Rds"
   vpc_security_group_ids = [aws_security_group.five.id]
@@ -55,7 +55,7 @@ resource "aws_instance" "three" {
 }
 
 resource "aws_instance" "four" {
-  ami             = "ami-03c7d01cf4dedc891"
+  ami             = "ami-04823729c75214919"
   instance_type   = "t2.micro"
   key_name        = "Rds"
   vpc_security_group_ids = [aws_security_group.five.id]
